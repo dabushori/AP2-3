4549b5272c65d858030593a3a9ecc0d538b864d6
= <dabushori@gmail.com>
2021-06-23 13:33:04 +0300

    Created the views folder

---
+++ app/src/main/java/com/example/ap2_3/views/Joystick.java
@@ -0,0 +1,10 @@
+package com.example.ap2_3.views;
+
+import android.content.Context;
+import android.view.View;
+
+public class Joystick extends View {
+    public Joystick(Context context) {
+        super(context);
+    }
+}
--- app/src/main/java/com/example/ap2_3/views/MainActivity.java
+++ app/src/main/java/com/example/ap2_3/views/MainActivity.java
@@ -1,7 +1,8 @@
-package com.example.ap2_3;
+package com.example.ap2_3.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.ap2_3.R;
 
 public class MainActivity extends AppCompatActivity {
 
